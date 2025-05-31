665d102d9ee28f2c0209f3138594f06b4b04d16f
Esteban-02 <OESTEBAN02@GMAIL.COM>
2025-05-31 14:05:53 -0500

    Cambios en el funcionamiento del frontend con cambios del BackEnd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ProductoListaComponent } from './producto-lista/producto-lista.component';
 
-export const routes: Routes = [];
+
+//http://localhost:4200     -> Es la ruta por defecto
+export const routes: Routes = [
+    {path:'productos', component: ProductoListaComponent},      //redirigue a http://localhost:4200/productos -> url por defecto
+    {path:'', redirectTo: 'productos', pathMatch: 'full'}       //Redirigue a la URL por defecto
+];
+
+/**
+ * NOTA:
+ * No usar los href de HTML porque refresca toda la pagina y no se estaran usando los path definidos aqui, 
+ * para eso se usa reuterLink 
+ */
+
